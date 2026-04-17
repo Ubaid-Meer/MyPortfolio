1fa0dcdb6dd26cdc81ff8f2bfbbd20ce6c633992
Ubaid-Meer <ubaid13733@gmail.com>
2026-04-17 09:52:52 +0800

    fixing v2  vercel routing

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+    base: '/',  // ← change from './' to '/'
+  build: {
+    outDir: 'build',
+  },
   plugins: [
     react(),
     tailwindcss(),
